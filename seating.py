76f3c9321ae204bc1afc2c9c989168bcbd82064c
Johan Lübcke <johan.lubcke@trioptima.com>
2014-02-20 19:06:44 +0100

    More

--- seating.py
+++ seating.py
@@ -1,29 +1,42 @@
 from ortools.constraint_solver import pywrapcp
 
 
+NUMBER_OF_PERSONS = 8
+NUMBER_OF_MEALS = 3
+NUMBER_OF_TABLES = 2
+
+
 def seating():
     # Constraint programming engine
-    solver = pywrapcp.Solver('Seating');
+    solver = pywrapcp.Solver('Seating')
 
-    # Decision variables
-    places = range(0, 4)
-    a = solver.IntVar(places, 'a')
-    b = solver.IntVar(places, 'b')
-    c = solver.IntVar(places, 'c')
-    d = solver.IntVar(places, 'd')
+    v = []
+    for m in range(NUMBER_OF_MEALS):
+        tmp = []
+        for p in range(NUMBER_OF_PERSONS):
+            tmp.append(solver.IntVar(range(NUMBER_OF_PERSONS), '<p%sm%s>' % (p, m)))
+        solver.Add(solver.AllDifferent(tmp))
+        v.append(tmp)
 
-    persons = [a, b, c, d]
-    solver.Add(solver.AllDifferent(persons))
+    for p1 in range(NUMBER_OF_PERSONS):
+#        for p2 in range(NUMBER_OF_PERSONS):
+
+            tmp = []
+            for m in v:
+                tmp.append(m[p1])
+            solver.Add(solver.AllDifferent(tmp))
 
     #sum_value = solver.Sum([kBase*kBase*kBase*t, kBase*kBase*r, kBase*u, e])
     #solver.Add(sum_terms == sum_value)
 
-    db = solver.Phase(persons, solver.INT_VAR_DEFAULT,
+    db = solver.Phase([p for p in m for m in v],
+                      solver.INT_VAR_DEFAULT,
                       solver.INT_VALUE_DEFAULT)
     solver.NewSearch(db)
 
     if solver.NextSolution():
-        print persons
+        for m in v:
+            print m
     else:
         print 'Cannot solve problem.'
 
